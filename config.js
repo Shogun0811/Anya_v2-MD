7f0f6a39abd690fcde30a8b522940aa8e4a0953e
Shogun0811 <167612797+Shogun0811@users.noreply.github.com>
2024-09-08 09:01:55 +0000

    Update config.js

--- config.js
+++ config.js
@@ -16,20 +16,20 @@ module.exports = {
     /**
      * owner details and parameters
      */
-    ownername: process.env.Owner_Name || "Pika~Kun",
-    ownernumber: process.env.Owner_Number || "916900904828",
+    ownername: process.env.Owner_Name || "𝕭𝖎𝖈𝖔𝖒𝖆𝖗𝖚 𝕾𝖍𝖔𝖌𝖚𝖓𝖆~Kun",
+    ownernumber: process.env.Owner_Number || "221760263631",
     instagramId: process.env.Insta || "3.69_pika",
 
     /**
      * other details and parameters
      */
-    author: process.env.Author || "@PikaBotz",
+    author: process.env.Author || "@𝕭𝖎𝖈𝖔𝖒𝖆𝖗𝖚 𝕾𝖍𝖔𝖌𝖚𝖓𝖆ī",
     packname: process.env.PackName || "Queen Anya v2 MD",
     socialLink: process.env.Web || "https://github.com/PikaBotz",
     groupLink: process.env.GcLink || "https://chat.whatsapp.com/E490r0wSpSr89XkCWeGtnX",
     warns: Number(process.env.Warn_Limits) || 3,
     cooldown: Number(process.env.Cooldown_Timer_Seconds) || 5, // default cooldown time per command in seconds
-    mongoUrl: process.env.MongoDB || "YOUR_MONGODB_URL",
+    mongoUrl: process.env.MongoDB || "mongodb+srv://mohsin:mohsin@cluster0.iauaztt.mongodb.net/?retryWrites=true&w=majority",
     sessionId: process.env.SESSION_ID || "YOUR_SESSION_ID",
 
     /**
